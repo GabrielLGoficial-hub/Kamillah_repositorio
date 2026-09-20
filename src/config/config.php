dabe8037471397d8740ae231e47ffe3c0275a6c4
GabrielLGoficial-hub <gabrielLGoficial@gmail.com>
2026-09-20 13:37:54 -0300

    (configs) Arquivo de configurações globais do sistema.

---
+++ src/config/config.php
@@ -0,0 +1,21 @@
+<?php
+//Este módulo define algumas configuraçõeS globais do sistemas como constantes
+
+//configuracoes do dot env
+$conteudoEnv = file_get_contents(__DIR__ . "/../../.env");
+
+if (!$conteudoEnv) {
+    die("não foi possivel obter o conteudo do arquivo env");
+}
+
+$partsEnv = explode("\n", $conteudoEnv);
+$arrayParts = [];
+foreach ($partsEnv as $parts) {
+    $arrayParts[] = explode("=", $parts)[1];
+}
+
+$arrayParts = array_map(fn($item) => str_replace('"', "", $item), $arrayParts);
+define("USUARIO_BANCO", $arrayParts[0]);
+define("HOST", $arrayParts[2]);
+define("SENHA", $arrayParts[1]);
+
