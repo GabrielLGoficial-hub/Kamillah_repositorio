3107aa573ec40e8a9cb6d6e712d4f6dd37fe6371
GabrielLGoficial-hub <gabrielLGoficial@gmail.com>
2026-09-21 19:23:02 -0300

    (funcionalidade) Criação das funções helpers da classe BD, função query e tratamento de erros de conexao.

--- src/classes/BD.php
+++ src/classes/BD.php
@@ -8,6 +8,7 @@ class BD
     private string $host;
     private string $banco;
     private string $codificacao;
+    private mysqli $mysqli;
     private static mixed $instacia = null;
 
     private function __construct(string $banco = "", string $codificacao = "UTF-8")
@@ -28,11 +29,66 @@ class BD
     {
         try {
             $mysql = new mysqli($this->host, $this->usuario, $this->senha, $this->banco);
-            if (!$mysql) {
-                throw new Exception("falha ao tentar conectar com o mysql", mysqli_errno($mysql));
-            }
-        } catch (Exception $e) {
+            $this->mysqli = $mysql;
+        } catch (mysqli_sql_exception $e) {
             echo $e->getMessage();
+        }
+    }
+
+    public function setCodificacao(string $codificacao)
+    {
+        try {
+            $this->mysqli->query("DO 1");
+            $this->mysqli->set_charset($codificacao);
+        } catch (mysqli_sql_exception $e) {
+            echo "Não é possivel modificar o charset do banco" . $this->mysqli->error;
+        }
+    }
+
+    public function getCodificacao()
+    {
+        return $this->codificacao;
+    }
+
+    public function getBanco()
+    {
+        return $this->banco;
+    }
+
+    public function query(string $sql, array $parametros = [])
+    {
+        try {
+            $stmt = $this->mysqli->prepare($sql);
+            $stmt->execute($parametros);
+
+            if (strpos($sql, "SELECT") === 0 or strpos($sql, "SHOW") === 0 or strpos($sql, "DESCRIBE") === 0) {
+                $result = $stmt->get_result();
+                return $result->fetch_assoc();
+            } else {
+                return $stmt->affected_rows;
+            }
+        } catch (mysqli_sql_exception $e) {
+            if (!$this->tratarTimeOut($e->getCode())) {
+                echo $e->getMessage();
+            }
+        }
+    }
+
+    private function tratarTimeOut(string $codigoException)
+    {
+        $arrayCodigo = [
+            2002,
+            2003,
+            2006,
+            2013,
+            3024
+        ];
+
+        if (in_array($codigoException, $arrayCodigo)) {
+            $this->mysqli->close();
+            $this->conectar();
+        } else {
+            return false;
         }
     }
 }
